52b810e127505842c6dbec05decc4471679a65e3
Emma Blott <blott23@gmail.com>
2021-02-18 12:00:57 +0000

    Initial commit for mongo

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'cb08c39938fd06726223832ad9d147c3'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'f84826df-eddd-4485-947f-30d0a5a8dd22'
+};
